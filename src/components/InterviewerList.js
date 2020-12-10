9fb47fce6910c062d7691a1efd1a4ebbdfc6a2ba
dlinardi <dlinardi98@gmail.com>
2020-12-09 22:02:52 -0500

    added all Appointment components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,13 +4,13 @@ import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
 
-  const fillInterviewerList = props.interviewers.map((interviewer) => {
+  const interviewers = props.interviewers.map(interviewer => {
     return <InterviewerListItem 
               key={interviewer.id} 
               name={interviewer.name}
               avatar={interviewer.avatar}
-              selected={interviewer.id === props.interviewer}
-              setInterviewer={props.setInterviewer} 
+              selected={interviewer.id === props.value}
+              setInterviewer={event => props.onChange(interviewer.id)} 
             />
   }); 
 
@@ -18,7 +18,7 @@ export default function InterviewerList(props) {
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {fillInterviewerList}
+        {interviewers}
       </ul>
     </section>
   );
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,7 +9,7 @@ export default function InterviewerListItem(props) {
   });
 
   return(
-    <li onClick={() => props.setInterviewer(props.name)} className={interviewerClass}>
+    <li onClick={props.setInterviewer} className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
