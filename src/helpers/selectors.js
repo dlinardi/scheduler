47a713c55c31d504be9ef0f3e81baf9845c9f187
dlinardi <dlinardi98@gmail.com>
2020-12-10 02:20:34 -0500

    implemented getAppointmentsForDay helper

---
+++ src/helpers/selectors.js
@@ -0,0 +1,20 @@
+export function getAppointmentsForDay(state, day) {
+  let appointments = []
+  const results = [];
+  
+  state.days.forEach((days) => {
+    if (days.name === day) {
+      appointments = days.appointments;
+    }
+  })
+
+  appointments.forEach((appointment) => {
+    for (const key in state.appointments) {
+      if (appointment === state.appointments[key].id) {
+        results.push(state.appointments[key]);
+      }
+    }
+  });
+
+  return results;
+}
